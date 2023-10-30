a8c76aafc2f53256ef971ab02643198a777f09d3
Pat McMpadden <manipulation2000@hotmail.com>
2023-10-30 11:41:54 -0500

    more refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,29 @@
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { GET_USERS_FETCH, getUsersFetch } from './store/actions'; // Modified path
+import { getUsersFetch } from './store/actions';
 import './App.css';
 
 function App() {
   const dispatch = useDispatch();
   const users = useSelector(state => state.users || []);
-  console.log("app user - ",users);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <button onClick={() => dispatch(getUsersFetch())}>Get users</button>
-        <ul>
-          {users.map(user => (<><li key={user.id}>{user.name}</li><ul>{user.email}</ul></>))}
-        </ul>
-      </header> 
+      <div className="App-header">
+        <button onClick={() => dispatch(getUsersFetch())}>Get Users</button>
+        {users.length > 0 && (
+          <ul>
+            {users.map(user => (
+              <li key={user.id}>
+                {user.name}
+                {user.email && <ul>{user.email}</ul>}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
 
 export default App;
-
-
-
-// import { useState } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { GET_USERS_FETCH, getUsersFetch } from './actions';
-// import './App.css'
-
-// function App() {
-//   const dispatch = useDispatch();
-//   const users = useSelector(state => state.myFirst.users);
-// console.log("app user - ",users);
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//       <button onClick={() => dispatch(getUsersFetch())}>Get users</button>
-//         <ul>
-//         {users.map(user => (<li key={user.id}>{user.name}</li>))}
-//         </ul>
-//       </header> 
-//     </div>
-//   )
-// };
-
-
-// export default App
-
-
-
-// working code
-// import { useState } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { GET_USERS_FETCH, getUsersFetch } from './actions';
-// import './App.css'
-
-// function App() {
-//   const dispatch = useDispatch();
-//   const users = useSelector(state => state.myFirst.users);
-// console.log("app user - ",users);
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//       <button onClick={() => dispatch(getUsersFetch())}>Get users</button>
-//         <ul>
-//         {users.map(user => (<li key={user.id}>{user.name}</li>))}
-//         </ul>
-//       </header> 
-//     </div>
-//   )
-// };
-
-
-// export default App
